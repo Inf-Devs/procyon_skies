e34a91ec3406d2fb44a0c4f59577db4192bbb4fc
Julian Will <LukeCastellan@users.noreply.github.com>
2019-10-08 12:49:43 -0400

    Players no longer start with 10k resources
    Left over from demo mode?

--- scripts/player.js
+++ scripts/player.js
@@ -57,7 +57,7 @@ function Player(name, colour, id) {
     this.type         = "player";
     this.score        = 0; // stats!
     this.exhaust      = Math.random() < 0.5 ? Particles.Bubble : Particles.Shrinking_diamond;
-    this.resources    = 10000;
+    this.resources    = 500;
     
     //orbit! not the KSP kind.
     this.orbiting_planet = default_planet;
